7fb7479e2e6c730bbb272876a5b8cd37cc8e5098
Phillip Henry <PhillHenry@gmail.com>
2022-06-09 17:32:25 +0100

    Refactored.

--- modules/core/src/main/scala/uk/co/odinconsultants/mss/StatefulStreams.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/mss/StatefulStreams.scala
@@ -2,6 +2,7 @@ package uk.co.odinconsultants.mss
 
 import fs2.Stream
 import cats.effect.{IO, Ref}
+import uk.co.odinconsultants.mss.StatefulStreams.WordCount
 
 object StatefulStreams {
 
@@ -9,16 +10,17 @@ object StatefulStreams {
 
   def update(old: WordCount, word: String): WordCount = old + (word -> (old.getOrElse(word, 0) + 1))
 
+  def updateIO(ref: Ref[IO, WordCount], word: String): IO[WordCount] = for {
+    _       <- IO.println(word)
+    changed <- ref.modify(old => (update(old, word), update(old, word)))
+  } yield changed
+
   def orderedStream(xs: List[String]): Stream[IO, WordCount] = {
     val initial: IO[Ref[IO, WordCount]] = Ref.of[IO, WordCount](Map.empty[String, Int])
     for {
-      ref   <- Stream.eval(initial)
-      word  <- Stream.emits(xs)
-      io: IO[WordCount] = for {
-        _ <- IO.println(word)
-        changed <- ref.modify(old => (update(old, word), update(old, word)))
-      } yield changed
-      result <- Stream.eval(io)
+      ref     <- Stream.eval(initial)
+      word    <- Stream.emits(xs)
+      result  <- Stream.eval(updateIO(ref, word))
     } yield {
       result
     }
