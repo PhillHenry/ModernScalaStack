7239660dba14b566074882d4c4a579bfdb81c31b
Phillip Henry <PhillHenry@gmail.com>
2022-07-02 09:01:45 +0100

    Where did that $ come from?!?

--- modules/core/src/test/scala/uk/co/odinconsultants/mss/StreamingWordCountSuite.scala
+++ modules/core/src/test/scala/uk/co/odinconsultants/mss/StreamingWordCountSuite.scala
@@ -6,7 +6,7 @@ import cats.effect.IO
 import cats.data.NonEmptyList
 import StreamingWordCount.WordCount
 
-object StreamingWordCountSuite$ extends SimpleIOSuite with Checkers:
+object StreamingWordCountSuite extends SimpleIOSuite with Checkers:
   test("stream produces word count") {
     val expectedWordCount: Map[String, Int] = Map("a" -> 1, "b" -> 2, "c" -> 3)
     val input: List[String]                 = List("c", "a", "c", "b", "b", "c")
