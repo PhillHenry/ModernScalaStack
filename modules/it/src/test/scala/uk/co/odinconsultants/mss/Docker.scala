702a9ae22be0b222e7b31a095621d0cb5123761c
PhillHenry@gmail.com <PhillHenry@gmail.com>
2022-08-16 09:31:15 +0100

    Hacking a generic start container.

--- modules/it/src/test/scala/uk/co/odinconsultants/mss/Docker.scala
+++ modules/it/src/test/scala/uk/co/odinconsultants/mss/Docker.scala
@@ -1,38 +1,82 @@
 package uk.co.odinconsultants.mss
 import cats.effect.{ExitCode, IO, IOApp}
+import cats.free.Free
 import com.github.dockerjava.api.DockerClient
+import com.github.dockerjava.api.command.{CreateContainerCmd, CreateContainerResponse}
+import com.github.dockerjava.api.model.{ExposedPort, Ports}
 import com.github.dockerjava.core.{DefaultDockerClientConfig, DockerClientImpl}
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient
-import cats.free.Free
-import com.github.dockerjava.api.command.CreateContainerResponse
+import uk.co.odinconsultants.mss.DockerMain.*
 
 object Docker extends IOApp.Simple {
 
   opaque type ApiVersion = String
 
-  def client(dockerHost: String, apiVersion: ApiVersion): IO[DockerClient] = IO {
-    val config: DefaultDockerClientConfig  = DockerMain.buildConfig(dockerHost, apiVersion)
-    val httpClient: ApacheDockerHttpClient = DockerMain.buildClient(config)
+  def initializeClient(dockerHost: String, apiVersion: ApiVersion): IO[DockerClient] = IO {
+    val config: DefaultDockerClientConfig  = buildConfig(dockerHost, apiVersion)
+    val httpClient: ApacheDockerHttpClient = buildClient(config)
     DockerClientImpl.getInstance(config, httpClient)
   }
 
+  def stopContainer(client: DockerClient, containerId: String): IO[Unit] = IO {
+    stopContainerWithId(client, containerId)
+  }
+
+  val host       = "unix:///var/run/docker.sock"
+  val apiVersion = "1.41"
+
   def run: IO[Unit] = {
-    val host       = "unix:///var/run/docker.sock"
-    val apiVersion = "1.41"
 
     val connect =
       Free.liftF(ConnectRequest[DockerClient](ConnectionURL(host)))
     val start   =
-      Free.liftF(StartRequest[CreateContainerResponse](ImageName(DockerMain.ZOOKEEPER_NAME)))
+      Free.liftF(
+        StartRequest[CreateContainerResponse](
+          ImageName(DockerMain.ZOOKEEPER_NAME),
+          Command("/bin/bash -c /entrypoint.sh /opt/bitnami/scripts/zookeeper/run.sh"),
+          List("ALLOW_ANONYMOUS_LOGIN=yes"),
+        )
+      )
 
-    def dockerInterpreter(x: ManagerRequest[?]) = x match {
-      case ConnectRequest(url) => IO(client(host, apiVersion))
-      case StartRequest(image) => IO(client(host, apiVersion))
-    }
-
-    client(host, apiVersion).handleErrorWith { (t: Throwable) =>
+    initializeClient(host, apiVersion).handleErrorWith { (t: Throwable) =>
       IO.println(s"Could not connect to host $host using API version $apiVersion") *>
         IO.raiseError(t)
     } >> IO.unit
+  }
+
+  def interpret(client: DockerClient): ManagerRequest[?] => IO[?] = _ match {
+    case ConnectRequest(url)           => initializeClient(url.toString, apiVersion)
+    case StartRequest(image, cmd, env) => start(client, image, cmd, env)
+    case StopRequest(containerId)      => IO(stopContainerWithId(client, containerId.toString))
+  }
+
+  def start(
+      dockerClient: DockerClient,
+      image: ImageName,
+      command: Command,
+      environment: Environment,
+  ): IO[CreateContainerResponse] = IO {
+    import scala.jdk.CollectionConverters.*
+    val tcp2181: ExposedPort = ExposedPort.tcp(2181)
+    val portBindings         = new Ports
+    portBindings.bind(tcp2181, Ports.Binding.bindPort(2181))
+
+    val config: CreateContainerCmd = dockerClient
+      .createContainerCmd(image.toString)
+      .withName(ZOOKEEPER_NAME)
+      .withAttachStdin(false)
+      .withAttachStdout(true)
+      .withAttachStderr(false)
+      .withEnv(environment.asJava)
+      .withCmd(command.toString.split(" ").toList.asJava)
+
+    val container: CreateContainerResponse = config
+      .withExposedPorts(tcp2181)
+      .withHostConfig(config.getHostConfig.withPortBindings(portBindings))
+      .exec
+
+    // start the container
+    dockerClient.startContainerCmd(container.getId).exec
+    container
   }
 }
--- modules/it/src/test/scala/uk/co/odinconsultants/mss/Domain.scala
+++ modules/it/src/test/scala/uk/co/odinconsultants/mss/Domain.scala
@@ -2,16 +2,26 @@ package uk.co.odinconsultants.mss
 
 opaque type ImageName     = String
 opaque type ConnectionURL = String
+opaque type ContainerId   = String
+opaque type Command       = String
+type Environment          = List[String]
 
 object ConnectionURL:
   def apply(x: String): ConnectionURL = x
 object ImageName:
   def apply(x: String): ImageName = x
+object ContainerId:
+  def apply(x: String): ContainerId = x
+object Command:
+  def apply(x: String): Command = x
 
 sealed abstract class ManagerRequest[A]
 
 case class ConnectRequest[A](url: ConnectionURL) extends ManagerRequest[A]
 
-case class StartRequest[A](image: ImageName) extends ManagerRequest[A]
+case class StartRequest[A](image: ImageName, command: Command, env: Environment)
+    extends ManagerRequest[A]
+
+case class StopRequest[A](image: ContainerId) extends ManagerRequest[A]
 
 object Domain {}
